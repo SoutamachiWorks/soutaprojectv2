432f4336835c67759b7f5982a6a365bf0ed6b06f
Souta <muhammad.mk32@gmail.com>
2025-06-10 04:26:53 +0700

    update contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Works from './Components/Works/Works'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Works />
+      <Contact />
     </div>
   )
 }
